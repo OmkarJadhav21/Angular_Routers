2fb9a76035726c451a8adbffe084effa5f41633e
MacMini_Techmodi <you@example.com>
2017-09-08 12:05:26 +0530

    Router:Default URL

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 // import { ForgetPassComponent } from './forget-pass/forget-pass.component';
 // import { DashComponent } from './dash/dash.component';
 import { LoginComponent } from './login/login.component';
@@ -9,7 +10,10 @@ import { ForgetCompoComponent } from './forget-compo/forget-compo.component';
 const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'dash/:user', component: DashComponent },
-    { path: 'forgetpass', component: ForgetCompoComponent }
+    { path: 'forgetpass', component: ForgetCompoComponent },
+
+    {path:'**',component:LoginComponent}//sequence of this statement should be at the last 
+    //because it says that anything above this is going to loginComponent 
 ];
 
 // tslint:disable-next-line:eofline
